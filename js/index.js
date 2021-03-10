19c05819ed78f3da8d71355822218ce203929a59
marina <marinaaltesmagret@gmail.com>
2021-03-10 13:11:24 +0100

    Finish iteration 2

--- js/index.js
+++ js/index.js
@@ -2,11 +2,26 @@ window.onload = () => {
   document.getElementById('start-button').onclick = () => {
     startGame();
   };
-
-
   const canvas = document.getElementById('canvas');
   const ctx = canvas.getContext("2d");
 
-  function startGame() {}
+  let car = {
+    x: 225,
+    y: 350,
+    w: 52,
+    h: 106,
+  };
+
+  function drawCar() {
+    ctx.drawImage(carImg, car.x, car.y, )
+  }
+  const carImg = new Image();
+  carImg.src = './images/car.png';  
+  carImg.onload = () => ctx.drawImage(carImg, car.x, car.y, car.w, car.h);
+  
+  function startGame() {
+    console.log("let's go!")
+  }
+
 };
 
